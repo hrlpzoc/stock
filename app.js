563ce00bd9d7eadaf1d7dce2a39627110ead2491
hrlpzoc <hrlpzoc@gmail.com>
2022-07-24 22:06:30 -0300

    cambios

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require ('express')
 
 const app = express()
@@ -5,9 +6,10 @@ const app = express()
 app.get('/', (req, res) =>{
     console.log ('peticiones recibidas')
 
-    res.send('<h1>hola mundo con nodemon</h1>')
+    res.send('<h1>hola mundo con nodemon conmigo hernán</h1>')
 })
+const PORT = process.env.PORT || 4000
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log ('servidor escuchando')
 })
