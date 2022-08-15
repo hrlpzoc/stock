6b1e2e10136a6efd232adc5e5dc26d854ae31a54
hrlpzoc <hrlpzoc@gmail.com>
2022-08-15 15:49:45 -0300

    Conexion con MDB + HTML CSS JS

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-require('dotenv').config()
+const { Console } = require('console');
 const express = require ('express')
+const mongoose = require('mongoose');
+const { restart } = require('nodemon');
+const path =require('path')
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) =>{
-    console.log ('peticiones recibidas')
+mongoose 
+.connect(
+    //sting de conexión de la base de datos
+    
+    'mongodb+srv://hernas:hernas2005@development.2xqf1jr.mongodb.net/stock-app?retryWrites=true&w=majority'
+    )
+    .then (result =>{
+        app.listen(PORT, () => {
+            console.log ('servidor escuchando en el puerto', PORT)  
+            //se ejecuta cuando el servidor esta escuchando en el puerto'
+        })
+    })
+    .then(result => (console.log('conectado a la base de datos')))
+    .catch(err => console.log(err))
 
-    res.send('<h1>hola mundo con nodemon conmigo</h1>')
-})
-const PORT = process.env.PORT || 4000
+    const productSchemas = new mongoose.Schema({
+        name: { type: String, required: true }, 
+        //price: { type: Number, required: true },
+        price: Number, 
 
-app.listen(PORT, () => {
-    console.log ('servidor escuchando')
-})
+    },
+    {timestamps: true}
+    )
+
+    const Product = mongoose.model('Product', productSchemas)
+    
+    //app.get('/api/v1/products', (req, res) => {});
+    
+    app.use(express.json())
+    
+
+    app.post('/api/v1/products', async (req, res, next) => {
+        //
+        const newProduct = new Product(req.body)
+        
+        //guarda en la base de datos
+        await newProduct.save()   
+        
+        .then(result => {res.status(201).json({ message: 'petición recibida' })})
+        .catch(err => console.log(err)) 
+    } ) 
+
+    app.use(express.static(path.join(__dirname, 'public')))
+
+
+    const PORT = process.env.PORT || 4000
---
+++ public/index.js
@@ -0,0 +1,22 @@
+const inputName = document.querySelector('#name');
+const inputPrice = document.querySelector('#price');
+const button = document.querySelector('#button');   
+
+button.addEventListener('click', (e) => {
+    console.log({ name: inputName.value, price: inputPrice.value });
+    const name = inputName.value;
+    const price = inputPrice.value;
+
+    fetch('http://localhost:4000/api/v1/products', {
+        method: 'POST',
+        headers: {
+        'content-type': 'application/json',
+        },
+        body : JSON.stringify({ 
+            name, 
+            price,
+        }),
+    })
+
+
+} );
